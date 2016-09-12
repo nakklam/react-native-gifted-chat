2a3d84cfa643618acaa6b585c9294cdb6ea01b7d
nakklam <teenueng@gmail.com>
2016-09-12 11:55:35 +0700

    Update Avatar.js

--- src/Avatar.js
+++ src/Avatar.js
@@ -22,7 +22,7 @@ export default class Avatar extends React.Component {
   }
 
   render() {
-    /*if (this.props.isSameUser(this.props.currentMessage, this.props.nextMessage) && this.props.isSameDay(this.props.currentMessage, this.props.nextMessage)) {
+    if (this.props.isSameUser(this.props.currentMessage, this.props.nextMessage) && this.props.isSameDay(this.props.currentMessage, this.props.nextMessage)) {
       return (
         <View style={[styles[this.props.position].container, this.props.containerStyle[this.props.position]]}>
           <GiftedAvatar
@@ -30,7 +30,7 @@ export default class Avatar extends React.Component {
           />
         </View>
       );
-    }*/
+    }
     return (
       <View style={[styles[this.props.position].container, this.props.containerStyle[this.props.position]]}>
         {this.renderAvatar()}
